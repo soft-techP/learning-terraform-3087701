c402845016c31ebcf6a4f32a3786cbf290140679
peterdb <p.deboeck@soft-tech.be>
2024-12-15 11:17:47 +0100

    Variables.tf file

--- variables.tf
+++ variables.tf
@@ -2,3 +2,19 @@
 #  description = "Type of EC2 instance to provision"
 #  default     = "t3.nano"
 #}
+
+variable "client_id" {
+  type = string
+}
+
+variable "client_secret" {
+  type = string
+}
+
+variable "subscription_id" {
+  type = string
+}
+
+variable "tenant_id" {
+  type = string
+}
